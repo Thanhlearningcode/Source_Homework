da27d3d555151c9909b87b6164ec76a72b43ae35
Thanhlearningcode <nguyenvanthanh76bk@gmail.com>
2025-03-05 21:25:27 -0800

    [Homework]-Add bai4B dang lam do t5

--- Homework4/ipb/Src/main.cpp
+++ Homework4/ipb/Src/main.cpp
@@ -7,15 +7,19 @@ void Byebye (void);
         atexit (Byebye);
         ipb::named_vector<int> acc("name", {4, 2, 1, 3});
         /*returns the sum over all the elements in the data container*/
-        std:: cout<< ipb::acculate (acc) <<std::end;
+        std:: cout << ipb::acculate (acc) <<std::end;
         /*return how many elements are stored in the container*/
-        std:: cout<< ipb::cout (vec,1) << std::end;
+        std:: cout << ipb::cout (vec,1) << std::end;
         /*returns true if all values in the container are even, false otherwise.*/
-        std:: cout<< ipb::all_even (vec) ? "TRUE" :"NO"<<std::endl;
+        std:: cout << ipb::all_even (vec) ? "TRUE" :"NO"<<std::endl;
         /*clamp all the values in the container to a given range [min, max]*/
-        std:: cout<< ipb::clamp(vec,0,15)<<"After clamping:"<<print(vec);
-        std:: cout<< ipb::
-
+        std:: cout << ipb::clamp (vec,0,15)<<"After clamping:"<<print(vec);
+        std:: cout << ipb::fill (vec,-99)<<std::endl;
+        std:: cout << ipb:: print(vec)<< std:: endl;
+        std:: cout << ipb::toupper(vec)<<std::endl;
+        std:: cout << ipb::sort (vec)<<std::endl;
+        std:: cout << ipb::rotate (vec,1)<< std::endl;
+        std:: cout << ipb::reverse (vec) << std::endl;
         return 1;
     }
 void Byebye (void) {
---
+++ Homework4/ipb/build/CMakeFiles/named_vector.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thanh/C_Plus_Plus/Homework/Homework4/ipb/Src/ibp.cpp" "/home/thanh/C_Plus_Plus/Homework/Homework4/ipb/build/CMakeFiles/named_vector.dir/Src/ibp.cpp.o"
+  "/home/thanh/C_Plus_Plus/Homework/Homework4/ipb/Src/main.cpp" "/home/thanh/C_Plus_Plus/Homework/Homework4/ipb/build/CMakeFiles/named_vector.dir/Src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
