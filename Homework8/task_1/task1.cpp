6ceb1d3dd0c7a1fa577d2153c0ffcf776a471d48
Thanhlearningcode <nguyenvanthanh76bk@gmail.com>
2025-03-10 01:05:57 -0700

    [Homework]-Done homework8

--- Homework8/task_1/task1.cpp
+++ Homework8/task_1/task1.cpp
@@ -1,20 +1,40 @@
-#include <bitset>
-#include <cstddef>
 #include <iostream>
-#include <vector>
-int main() {
+#include <unistd.h>
+#include <sys/wait.h>
+#include <csignal>
 
-  double data[80 * 102400 / 8];
-  int j = 0;
-  while (true) {
-    for (int i = j * 102400 / 8; i < (j + 1) * 102400 / 8; ++i) {
-      data[i] = 1.2;
+void allocate_on_stack_until_crash() {
+    const int KiB = 1024;
+    int size = 0;
+
+    while (true) {
+        size += 100 * KiB;  //< Tăng mỗi lần 100KiB
+        std::cerr << size / KiB << " KiB allocated on stack" << std::endl;
+
+        char arr[size]; //< Cấp phát trên stack, gây tràn stack sau một thời gian
+
+        arr[0] = 'a'; //< Giả lập để compiler không bỏ qua
     }
-    ++j;
-    std::cerr << (j + 1) * 102400 / 1024 << "[KiB]"
-              << " "
-              << "Allocated in the stack" << std::endl;
-  }
+}
 
-  return 0;
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) { 
+        std::cout << "Child process started. PID: " << getpid() << std::endl;
+        while (true) {
+            sleep(1);
+            std::cout << "Child process (" << getpid() << ") is still running..." << std::endl;
+        }
+    } else if (pid > 0) { 
+        std::cout << "Parent process started. PID: " << getpid() << ", Child PID: " << pid << std::endl;
+        sleep(2); 
+        allocate_on_stack_until_crash(); // Cố tình gây crash
+
+        wait(nullptr); // Đợi con 
+    } else {
+        std::cerr << "Fork failed!" << std::endl;
+    }
+
+    return 0;
 }
